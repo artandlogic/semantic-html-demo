04e0349765f80b62362f33b53876cefc62645d1d
Paul Hendry <phendry@artandlogic.com>
2023-03-27 10:51:21 -0700

    Fix page base URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     app: {
+        baseURL: "/semantic-html-demo/",
+        cdnURL: "https://artandlogic.github.io/semantic-html-demo/",
         head: {
             link: [
                 { rel: "icon", type: "image/x-icon", href: "/images/logo.png" },
             ],
+            title: "Semantic HTML Demo",
         },
     },
     postcss: {
@@ -12,6 +15,7 @@ export default defineNuxtConfig({
             "postcss-nesting": {},
         },
     },
+    ssr: false,
     typescript: { shim: false },
     vite: {
         optimizeDeps: {
