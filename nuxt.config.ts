cdb65a213dc2577fad58f2134a7e52cec7fe2a7b
Paul Hendry <phendry@artandlogic.com>
2023-03-27 10:29:34 -0700

    Replace favicon with logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    typescript: { shim: false },
+    app: {
+        head: {
+            link: [
+                { rel: "icon", type: "image/x-icon", href: "/images/logo.png" },
+            ],
+        },
+    },
     postcss: {
         plugins: {
             "postcss-nesting": {},
         },
     },
+    typescript: { shim: false },
     vite: {
         optimizeDeps: {
             exclude: ["@highlightjs/vue-plugin"],
